185c6bc830a311d1af0c070912270e1e1c9ae6e4
Gustavo Clemente <gustavo.clementelopes@gmail.com>
2023-10-23 16:33:16 -0300

    Add alb-controller

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+provider "aws" {
+  profile = "terraform-user"
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--profile", "terraform-user"]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", "tech_challenge_cluster", "--profile", "terraform-user"]
+      command     = "aws"
+    }
+  }
+}
+
 terraform {
   required_providers {
     aws = {
@@ -7,37 +33,23 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-west-2"
-}
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "eks_cluster_vpc"
   cidr = "10.0.0.0/16"
 
-  azs                           = ["us-west-2a", "us-west-2b"]
-  public_subnets                = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets               = ["10.0.3.0/24", "10.0.4.0/24"]
-  enable_nat_gateway            = true
-  single_nat_gateway            = true
-  enable_dns_hostnames          = true
-  manage_default_security_group = true
-  default_security_group_name   = "vpc-serverless-security-group"
+  azs                = ["us-west-2a", "us-west-2b"]
+  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets    = ["10.0.3.0/24", "10.0.4.0/24"]
+  enable_nat_gateway = true
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/vpc-serverless" = "shared"
-    "kubernetes.io/role/elb"               = "1"
+    "kubernetes.io/role/elb" = "1"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/vpc-serverless" = "shared"
-    "kubernetes.io/role/internal-elb"      = "1"
-  }
-
-  tags = {
-    "kubernetes.io/cluster/vpc-serverless" = "shared"
+    "kubernetes.io/role/internal-elb" = "1"
   }
 }
 
@@ -45,17 +57,22 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name                         = "tech_challenge_cluster"
-  cluster_version                      = "1.28"
-  cluster_endpoint_public_access       = true
-  cluster_endpoint_public_access_cidrs = ["177.33.157.153/32"]
+  cluster_name    = "tech_challenge_cluster"
+  cluster_version = "1.28"
 
-  subnet_ids = flatten([module.vpc.public_subnets, module.vpc.private_subnets])
-  vpc_id     = module.vpc.vpc_id
+  cluster_endpoint_public_access       = true
+
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
+  vpc_id                   = module.vpc.vpc_id
 
   cluster_addons = {
-    kube-proxy = {}
-    vpc-cni    = {}
+    kube-proxy = {
+      
+    }
+    vpc-cni = {
+      
+    }
     coredns = {
       configuration_values = jsonencode({
         computeType = "Fargate"
@@ -80,7 +97,91 @@ module "eks" {
           namespace = "kube-system"
         }
       ]
-      subnets = flatten([module.vpc.private_subnets])
     }
+  }
+}
+
+module "lb_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                              = "role_eks_lb"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+resource "kubernetes_service_account" "service-account" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+      "app.kubernetes.io/component" = "controller"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn"               = module.lb_role.iam_role_arn
+      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+    }
+  }
+
+  depends_on = [module.eks]
+}
+
+resource "helm_release" "alb-controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  depends_on = [
+    kubernetes_service_account.service-account
+  ]
+
+  set {
+    name  = "region"
+    value = "us-west-2"
+  }
+
+  set {
+    name  = "vpcId"
+    value = module.vpc.vpc_id
+  }
+
+  set {
+    name  = "image.repository"
+    value = "602401143452.dkr.ecr.us-west-2.amazonaws.com/amazon/aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "clusterName"
+    value = "tech_challenge_cluster"
+  }
+}
+
+resource "kubernetes_namespace" "monitoring_tech_challenge" {
+  metadata {
+    annotations = {
+      name = "monitoring-tech-challenge"
+    }
+
+    labels = {
+      mylabel = "monitoring-tech-challenge"
+    }
+
+    name = "monitoring-tech-challenge"
   }
 }
