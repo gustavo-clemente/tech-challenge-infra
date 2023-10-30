5b246ad58fd1464179221c58260a41fa11b4eefc
Gustavo Clemente <gustavo.clementelopes@gmail.com>
2023-10-30 10:55:44 -0300

    remove 'depends on'

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -6,7 +6,6 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  depends_on = [module.vpc] 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
   cluster_endpoint_public_access = var.cluster_endpoint_public_access
